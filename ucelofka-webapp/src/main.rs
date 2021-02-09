ff719e924c964c80d3c9feecb6ba059bd79a4fac
Stepan Henek <stepan@henek.name>
2021-02-10 00:58:46 +0100

    feature: reduce wasm binary size

--- ucelofka-webapp/src/main.rs
+++ ucelofka-webapp/src/main.rs
@@ -1,11 +1,12 @@
 #![recursion_limit = "4096"]
 mod components;
 
-use console_error_panic_hook::hook;
-use std::panic;
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 fn main() {
-    panic::set_hook(Box::new(hook));
+    #[cfg(debug_assertions)]
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     yew::start_app::<components::App>();
 }
